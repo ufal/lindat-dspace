0508d3db01a154bbceefd3f0a7d8bb42cf1cc53d
Cyprian Laskowski <cyp@dspace.lindat.dev>
2017-03-07 14:52:29 +0100

    Checked JS message usage as well

--- utilities/project_helpers/scripts/check-message-usages.py
+++ utilities/project_helpers/scripts/check-message-usages.py
@@ -2,6 +2,7 @@
 
 import sys
 import subprocess
+import codecs
 import os
 import re
 import xml.etree.ElementTree as xml
@@ -10,58 +11,107 @@ from check_message_lib import find_language_file_name, root_directory
 
 language = sys.argv[1]
 
-prefixes = ['xmlui', 'homepage', 'input_forms', 'org.dspace', 'PiwikStatisticsTransformer', 'UFAL.firstpage']
-grep_command = 'grep -R -P "[>\'\\"](' + '|'.join(prefixes) + ')\\." --include=*.java --include=*.xsl --include=*.xmap --include=*.xslt --include=input-forms.xml'
 line_regexp = r'^(.+?):(.*)$'
-prefix_regexp = "[>'\"]((?:" + "|".join(prefixes) + ")\..+?)[<'\"]"
 
-os.chdir(root_directory)
-output = subprocess.check_output(grep_command, shell=True)
-output_lines = output.strip().split('\n')
+def find_xml_prefixes_and_files():
 
-message_prefixes = set()
-for grep_line in output_lines:
-    line_match = re.search(line_regexp, grep_line, re.U)
-    (file_name, line) = line_match.groups()
-    match_tuples = re.findall(prefix_regexp, line, re.U)
-    for match_tuple in match_tuples:
-        prefix = match_tuple
-        message_prefixes.add((prefix, file_name))
+    prefixes = ['xmlui', 'homepage', 'input_forms', 'org.dspace', 'PiwikStatisticsTransformer', 'UFAL.firstpage']
+    grep_command = 'grep -R -P "[>\'\\"](' + '|'.join(prefixes) + ')\\." --include=*.java --include=*.xsl --include=*.xmap --include=*.xslt --include=input-forms.xml --exclude-dir=*/target/* *'
+    prefix_regexp = "[>'\"]((?:" + "|".join(prefixes) + ")\..+?)[<'\"]"
 
-message_prefixes = sorted(message_prefixes, key=lambda x: -len(x[0]))
+    os.chdir(root_directory)
+    with open(os.devnull, 'w') as devnull:
+        output = subprocess.check_output(grep_command, shell=True, stderr=devnull)
+    output_lines = output.strip().split('\n')
+    message_prefixes = set()
+    for grep_line in output_lines:
+        line_match = re.search(line_regexp, grep_line, re.U)
+        (file_name, line) = line_match.groups()
+        match_tuples = re.findall(prefix_regexp, line, re.U)
+        for match_tuple in match_tuples:
+            prefix = match_tuple
+            message_prefixes.add((prefix, file_name))
 
-file_name = find_language_file_name(language)
-results = {'yes':[], 'no':[], 'partial':[]}
-root = xml.parse(file_name)
-messages = root.findall('*')
-print 'Checking message usage for the ' + str(len(messages)) + ' messages in ' + file_name + ' ...'
-for message in root.findall('*'):
-    key = message.get('key')
-    found_file_name = None
-    for (prefix, file_name) in message_prefixes:
-        if (key == prefix):
-            results['yes'].append((key, file_name))
-            found_file_name = file_name
-            break
-        elif (key.startswith(prefix)):
-            results['partial'].append((key, prefix, file_name))
-            found_file_name = file_name
-            break
-    if (found_file_name is None):
-        results['no'].append(key)
-    ## hmm, what about messages not starting with "xmlui", like "input-forms", etc ...
+    return sorted(message_prefixes, key=lambda x: -len(x[0]))
+
+def add_xml_results(language, results):
+    message_prefixes = find_xml_prefixes_and_files()
+    file_name = find_language_file_name(language, 'xml')
+    root = xml.parse(file_name)
+    messages = root.findall('*')
+    print 'Checking message usage for the ' + str(len(messages)) + ' messages in ' + file_name + ' ...'
+    for message in messages:
+        key = message.get('key')
+        result = {'type':'xml', 'match':'no', 'key':key, 'file_name':None, 'prefix':None}
+        for (prefix, file_name) in message_prefixes:
+            if (key == prefix):
+                result['match'] = 'full'
+                result['file_name'] = file_name
+                break
+            elif (key.startswith(prefix)):
+                result['match'] = 'partial'
+                result['file_name'] = file_name
+                result['prefix'] = prefix
+                break
+        results.append(result)
+
+def add_js_results(language, results):
+    message_file_name = find_language_file_name(language, 'js')
+    message_file = codecs.open(message_file_name, 'r', 'UTF-8')
+    message_regexp = '^"(.*?)": ".*?",?$'
+    in_messages_flag = False
+    for line in message_file:
+        message_match = re.search(message_regexp, line.strip(), re.U)
+        if (message_match):
+            key = message_match.group(1)
+            result = {'type':'js', 'match':'no', 'key':key, 'file_name':None, 'prefix':None}
+            grep_command = 'grep -R -P "(\\\\$|jQuery)\\.i18n\._\\([\'\\"]' + key + '[\'\\"][),]" --include=*.js --include=*.html --exclude-dir=*/target/* *'
+            os.chdir(root_directory)
+            try:
+                with open(os.devnull, 'w') as devnull:
+                    output = subprocess.check_output(grep_command, shell=True, stderr=devnull)
+                output_lines = output.strip().split('\n')
+                line_match = re.search(line_regexp, output_lines[0], re.U)
+                (file_name, line) = line_match.groups()
+                result['match'] = 'full'
+                result['file_name'] = file_name
+            except subprocess.CalledProcessError as e:
+                if e.returncode > 1:
+                    raise
+            results.append(result)
+
+results = []
+add_xml_results(language, results)
+add_js_results(language, results)
+
+results_xml_no = [result for result in results if result['type'] == 'xml' and result['match'] == 'no']
+results_xml_partial = [result for result in results if result['type'] == 'xml' and result['match'] == 'partial']
+results_xml_full = [result for result in results if result['type'] == 'xml' and result['match'] == 'full']
+
+results_js_no = [result for result in results if result['type'] == 'js' and result['match'] == 'no']
+results_js_full = [result for result in results if result['type'] == 'js' and result['match'] == 'full']
 
 print ''
-print 'Message keys with no match (' + str(len(results['no'])) + '):'
-for key in sorted(results['no']):
-    print '  ' + key
+print 'XML message keys with no match (' + str(len(results_xml_no)) + '):'
+for result in sorted(results_xml_no, key=lambda x: x['key']):
+    print '  ' + result['key']
+    
+print ''
+print 'XML message keys with partial match (' + str(len(results_xml_partial)) + '):'
+for result in sorted(results_xml_partial, key=lambda x: x['key']):
+    print '  ' + result['key'] + '  (' + result['prefix'] + ')  [' + result['file_name'] + ']'
 
 print ''
-print 'Message keys with partial match (' + str(len(results['partial'])) + '):'
-for (key, prefix, file_name) in sorted(results['partial'], key=lambda x: x[0]):
-    print '  ' + key + '  (' + prefix + ')  [' + file_name + ']'
+print 'XML message keys with full match (' + str(len(results_xml_full)) + '):'
+for result in sorted(results_xml_full, key=lambda x: x['key']):
+    print '  ' + result['key'] + '  [' + result['file_name'] + ']'
 
 print ''
-print 'Message keys with full match (' + str(len(results['yes'])) + '):'
-for (key, file_name) in sorted(results['yes'], key=lambda x: x[0]):
-    print '  ' + key + '  [' + file_name + ']'
+print 'JS message keys with no match (' + str(len(results_js_no)) + '):'
+for result in sorted(results_js_no, key=lambda x: x['key']):
+    print '  ' + result['key']
+    
+print ''
+print 'JS message keys with match (' + str(len(results_js_full)) + '):'
+for result in sorted(results_js_full, key=lambda x: x['key']):
+    print '  ' + result['key'] + '  [' + result['file_name'] + ']'
--- utilities/project_helpers/scripts/check_message_lib.py
+++ utilities/project_helpers/scripts/check_message_lib.py
@@ -6,27 +6,34 @@ import codecs
 
 script_directory = os.path.dirname(os.path.realpath(__file__))
 root_directory = script_directory + '/../../..'
-i18n_directory = root_directory + '/dspace-xmlui/src/main/webapp/i18n'
-en_joint_file_name = '/tmp/messages-en.xml'
+xml_i18n_directory = root_directory + '/dspace-xmlui/src/main/webapp/i18n'
+js_i18n_directory = root_directory + '/dspace-xmlui/src/main/webapp/themes/UFAL/lib/js/messages'
+xml_en_joint_file_name = '/tmp/messages-en.xml'
 
-def find_language_file_name(language):
-    if (language != 'en'):
-        file_name = i18n_directory + '/messages_' + language + '.xml'
-    else:
-        file_name = en_joint_file_name
-        _create_en_joint_file()
+def find_language_file_name(language, kind):
+    if (kind == 'xml'):
+        if (language != 'en'):
+            file_name = xml_i18n_directory + '/messages_' + language + '.xml'
+        else:
+            file_name = xml_en_joint_file_name
+            _create_xml_en_joint_file()
+    elif (kind == 'js'):
+        if (language != 'en'):
+            file_name = js_i18n_directory + '/messages_' + language + '.js'
+        else:
+            file_name = js_i18n_directory + '/messages.js'
     return file_name
 
 ## Merge together all messages.xml into one temporary messages-en.xml.
 ## Avoids xml parsing to prevent namespace complications.
-def _create_en_joint_file():
+def _create_xml_en_joint_file():
     en_file_names = set()
     for (dpath, dnames, fnames) in os.walk(root_directory):
         for fname in [os.path.join(dpath, fname) for fname in fnames]:
             if (fname.endswith('/messages.xml')):
                 en_file_names.add(fname)
     print 'Constructing temporary /tmp/messages_en.xml from all messages.xml:\n  ' + '\n  '.join(en_file_names) + '\n'
-    en_joint_file = codecs.open(en_joint_file_name, 'w', 'UTF-8')
+    en_joint_file = codecs.open(xml_en_joint_file_name, 'w', 'UTF-8')
     for (index, en_file_name) in enumerate(en_file_names):
         en_file = codecs.open(en_file_name, 'r', 'UTF-8')
         if (index == 0):
